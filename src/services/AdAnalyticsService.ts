134f7c2bc21dedc536d30a3dd3e08822ddc3254f
hariom936 <hariomverma968@gmail.com>
2026-03-31 00:09:21 +0530

    AdAnalytics Module Api Done

--- src/services/AdAnalyticsService.ts
+++ src/services/AdAnalyticsService.ts
@@ -1,40 +1,40 @@
-// import { Service } from "typedi";
-// import { AdAnalyticsModel } from "../model/AdAnalytics";
+import { Service } from "typedi";
+import { AdAnalyticsModel } from "../model/AdAnalytics";
 
-// @Service()
-// export class AdAnalyticsService {
+@Service()
+export class AdAnalyticsService {
 
-//   async trackView(userId: string, adId: string) {
-//     return AdAnalyticsModel.updateOne(
-//       { userId, adId },
-//       { $setOnInsert: { viewed: true } },
-//       { upsert: true }
-//     );
-//   }
+  async trackView(userId: string, adId: string) {
+    return AdAnalyticsModel.updateOne(
+      { userId, adId },
+      { $setOnInsert: { viewed: true } },
+      { upsert: true }
+    );
+  }
 
-//   async trackClick(userId: string, adId: string) {
-//     return AdAnalyticsModel.updateOne(
-//       { userId, adId },
-//       { $set: { clicked: true, viewed: true } },
-//       { upsert: true }
-//     );
-//   }
+  async trackClick(userId: string, adId: string) {
+    return AdAnalyticsModel.updateOne(
+      { userId, adId },
+      { $set: { clicked: true, viewed: true } },
+      { upsert: true }
+    );
+  }
 
-//   async getStats(adId: string) {
-//     const views = await AdAnalyticsModel.countDocuments({
-//       adId,
-//       viewed: true,
-//     });
+  async getStats(adId: string) {
+    const views = await AdAnalyticsModel.countDocuments({
+      adId,
+      viewed: true,
+    });
 
-//     const clicks = await AdAnalyticsModel.countDocuments({
-//       adId,
-//       clicked: true,
-//     });
+    const clicks = await AdAnalyticsModel.countDocuments({
+      adId,
+      clicked: true,
+    });
 
-//     return {
-//       views,
-//       clicks,
-//       ctr: views ? (clicks / views) * 100 : 0,
-//     };
-//   }
-// }
+    return {
+      views,
+      clicks,
+      ctr: views ? (clicks / views) * 100 : 0,
+    };
+  }
+}
